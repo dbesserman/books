96738dba756779fb92e4088433ce36eec62b16f7
David Besserman <david.besserman@gmail.com>
2017-08-19 14:52:21 +0200

    [dprb] observer

---
+++ design_patterns_in_ruby/ch5_observer/observer_with_inheritance.rb
@@ -0,0 +1,49 @@
+class Subject
+  def initialize
+    @observers = []
+  end
+
+  def notify_observers
+    @observers.each { |obs| obs.update(self) }
+  end
+
+  def add_observer(observer)
+    @observers << observer
+  end
+
+  def delete_observer(observer)
+    @observers.delete(observer)
+  end
+end
+
+class Employee < Subject
+  attr_reader   :name
+  attr_accessor :title, :salary
+
+  def initialize(name, title, salary)
+    @name      = name
+    @title     = title
+    @salary    = salary
+
+    super
+  end
+
+  def salary=(new_salary)
+    @salary = new_salary
+
+    notify_observers
+  end
+end
+
+class Payroll
+  def update(changed_employee)
+    puts "Cut a new check for #{changed_employee.name}!"
+    puts "His salary is now #{changed_employee.salary}!"
+  end
+end
+
+class TaxMan
+  def update(changed_employee)
+    puts "Send #{changed_employee.name} a new tax bill!"
+  end
+end
