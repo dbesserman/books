8cff73164fdcb709e6cbc410deff7e1fa15fab5e
David Besserman <david.besserman@gmail.com>
2017-08-26 12:57:05 +0200

    [dprb] composite

---
+++ design_patterns_in_ruby/ch6_composite/composite_with_operators.rb
@@ -0,0 +1,81 @@
+class Task
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+  end
+
+  def get_time_required
+    0.0
+  end
+end
+
+class CompositeTask < Task
+  attr_reader :name
+
+  def initialize(name)
+    super name
+    @sub_tasks = []
+  end
+
+  def <<(task)
+    sub_tasks << task
+  end
+  
+  def [](index)
+    sub_tasks[index]
+  end
+
+  def []=(index, new_value)
+    sub_tasks[index] = new_value
+  end
+
+  def remove_sub_task(task)
+    sub_tasks.delete(task)
+  end
+
+  def get_time_required
+    time = 0.0
+    sub_tasks.each { |task| time += task.get_time_required }
+    time
+  end
+end
+
+class AddDryIngredientsTask < Task
+  def initialize
+    super "Add dry ingredients"
+  end
+
+  def get_time_required
+    1.0
+  end
+end
+
+class AddLiquidIngredientsTask
+  def initialize
+    super "Add liquid ingredients"
+  end
+
+  def get_time_required
+    1.0
+  end
+end
+
+class MixTask < Task
+  def initialize
+    super "Mix that batter up!"
+  end
+
+  def get_time_required
+    3.0
+  end
+end
+
+class MakeBatterTask < CompositeTask
+  def initialize
+    super "Make batter"
+    add_sub_task(AddDryIngredientsTask.new)
+    add_sub_task(AddLiquidIngredientsTask.new)
+    add_sub_task(MixTask.new)
+  end
+end
