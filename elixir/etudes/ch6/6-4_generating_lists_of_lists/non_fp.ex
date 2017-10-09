ced8d3d6bc9cdf6ca4f3ce50397788ff22c2fc96
David Besserman <david.besserman@gmail.com>
2017-10-09 13:48:15 +0200

    [intro_ex] etude 6-4

--- elixir/etudes/ch6/6-4_generating_lists_of_lists/non_fp.ex
+++ elixir/etudes/ch6/6-4_generating_lists_of_lists/non_fp.ex
@@ -1,25 +1,24 @@
 defmodule NonFP do
   def generate_pockets(teeth_list, probability) do
+    :random.seed(:erlang.now)
     generate_pockets(teeth_list, probability, [])
   end
 
   def generate_pockets([], _probability, result) do
-    result
+    Enum.reverse result
   end
 
   def generate_pockets([?F|t], probability, result) do
-    generate_pockets(t, probability,[[0] | result]
+    generate_pockets(t, probability,[[0] | result])
   end
 
-  def generate_pockets(?T|t, probability, result) do
+  def generate_pockets([?T|t], probability, result) do
     tooth = generate_tooth(probability)
 
-    generate_pockets(t, probability,[tooth | result]
+    generate_pockets(t, probability,[tooth | result])
   end
 
   def generate_tooth(probability) do
-    :random.seed(:erlang.now)
-  
     cond do
       :random.uniform < probability -> base_depth = 2
       true                          -> base_depth = 3
@@ -33,6 +32,8 @@ defmodule NonFP do
   end
 
   def generate_tooth(base_depth, nb_of_items, list) do
+    depth = base_depth + :random.uniform(3) - 2
     
+    generate_tooth(base_depth, nb_of_items - 1, [depth | list])
   end
 end
